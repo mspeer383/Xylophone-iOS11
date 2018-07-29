3c31d7b5b895bf6385daddab9915360a07a3b9db
“Michael <michael_speer@comcast.com>
2018-07-29 17:35:50 -0500

    init

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
-    
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    var player : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +20,22 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        playSound(soundID: Int(sender.tag))
         
+    }
+    
+    func playSound(soundID : Int ) {
+        let url = Bundle.main.url(forResource: "note\(soundID)", withExtension: "wav")!
         
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
     
   
